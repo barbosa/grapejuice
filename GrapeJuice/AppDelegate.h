af36a1dcdae924f5c94b1945aed7a06d20d2f92e
Bruno Torres <bruno.torres@me.com>
2013-12-16 15:07:42 -0200

    Added empty iOS application and Xcode workspace to repository.

---
+++ GrapeJuice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GrapeJuice
+//
+//  Created by Bruno Torres on 12/16/13.
+//  Copyright (c) 2013 Globo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
