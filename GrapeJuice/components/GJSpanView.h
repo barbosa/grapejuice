789318a389e8c515ed3ab861fdef5a6b76871d4d
Gustavo Barbosa <gustavocsb@gmail.com>
2014-02-05 21:37:58 -0200

    GJSpanView initial structure

--- GrapeJuice/components/GJSpanView.h
+++ GrapeJuice/components/GJSpanView.h
@@ -8,6 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
+@class GJLayout;
+
 @interface GJSpanView : UIView
+
+@property (nonatomic, strong) GJLayout* layout;
 
 @end
--- GrapeJuice/components/GJSpanView.m
+++ GrapeJuice/components/GJSpanView.m
@@ -8,6 +8,28 @@
 
 #import "GJSpanView.h"
 
+// grapejuice
+#import "UIView+HtmlLayout.h"
+#import "GJLayout.h"
+#import "GJStyle.h"
+#import "GJStylesheet.h"
+
+@interface GJSpanView ()
+
+@property( nonatomic, readwrite, strong )GJStyle* computedStyle;
+
+@end
+
 @implementation GJSpanView
+
+-( void )layoutSubviews
+{
+    self.computedStyle = [_layout.stylesheet computedStyleForTag: _layout.tag classes: _layout.classes];
+    
+    self.layer.borderColor = _computedStyle.borderColor.CGColor;
+    self.layer.borderWidth = _computedStyle.borderWidth;
+    self.backgroundColor = _computedStyle.backgroundColor;
+    [self layoutSubviewsInsideOut];
+}
 
 @end
